79ce76f41bfd62ffd1818ec9c128df9d24202243
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-06-25 19:39:07 +0530

    largestSubarraywithKsum

---
+++ largestSubarrayKsum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+  public:
+    int maxLen(vector<int>& arr, int n) {
+        unordered_map<int, int> mpp;  
+        int maxi = 0;
+        int sum = 0;  
+    
+        for (int i = 0; i < n; i++) {
+            sum += arr[i]; 
+            if (sum == 0) {
+                maxi = i + 1; 
+            } else {
+                if (mpp.find(sum) != mpp.end()) {
+                    maxi = max(maxi, i - mpp[sum]);
+                } else {
+                    mpp[sum] = i;
+                }
+            }
+        }
+    
+        return maxi;
+    }
+};
