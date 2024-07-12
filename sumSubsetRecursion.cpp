fa2da5b90a7c102b3e4c949b875f0902eee5e80c
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-07-12 12:10:54 +0530

    sumSubsetRecursion

---
+++ sumSubsetRecursion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    void func(int ind, int sum, vector<int>&arr, int n, vector<int>&sumSubset){
+        if(ind == n){
+            sumSubset.push_back(sum);
+            return;
+        }
+        //pick the element
+        func(ind+1, sum + arr[ind], arr, n, sumSubset);
+        //do not pick the element
+        func(ind+1, sum, arr, n, sumSubset);
+    }
+    vector<int> subsetSums(vector<int> arr, int n) {
+        vector<int>sumSubset;
+        func(0,0,arr,n,sumSubset);
+        return sumSubset;
+    }
+};
